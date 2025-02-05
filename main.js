08c616b4c144818adc6c4ae732bcc3ae7ff1af6a
Pooja <poojasihag.dev@gmail.com>
2025-02-05 11:35:03 +0530

    completed faqsPage styling

--- main.js
+++ main.js
@@ -3,3 +3,23 @@
 window.addEventListener('scroll', ()=>{
     document.querySelector('nav').classList.toggle('window-scroll',window.scrollY > 0)
 })
+
+
+
+// Show / hide faq answers
+
+const faqs = document.querySelectorAll('.faq');
+ 
+faqs.forEach(faq=>{
+    faq.addEventListener('click',()=>{
+        faq.classList.toggle('open');
+
+        //change icon
+        const icon= faq.querySelector('.faqs__icon i');
+        if(icon.className === 'uil uil-plus'){
+            icon.className = 'uil uil-minus';
+        }else {
+            icon.className = 'uil uil-plus';
+        }
+    })
+})
